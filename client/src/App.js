59d219074fe0547a42f0503bc49ef5022095472b
Vishwa Mehta <vishwamehta2002@gmail.com>
2022-03-09 14:11:52 +0530

    basic consumer page added :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Landing from './Components/Landing';
+import Consumer from './Components/Consumer';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Landing/>
+      <Consumer/>
     </div>
   );
 }
---
+++ client/src/Components/Consumer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ConsumerForm from './consumer/ConsumerForm'
+
+function Consumer() {
+  return (
+    <div>
+        <ConsumerForm/>
+    </div>
+  )
+}
+
+export default Consumer
